db306ae67b8177256f070ec186a7a39e17219b12
alba <alballubani19@gmail.com>
2024-06-19 23:20:00 +0200

    updated layout componentes

--- src/app/lib/components/layouts/header/header.component.ts
+++ src/app/lib/components/layouts/header/header.component.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Store } from '@ngrx/store';
+import { AuthActions } from '@store/auth/auth.actions';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+  private readonly _store = inject(Store);
 
+  logout() {
+    this._store.dispatch(AuthActions.logout());
+  }
 }
--- src/app/lib/components/layouts/main-layout/main-layout.component.ts
+++ src/app/lib/components/layouts/main-layout/main-layout.component.ts
@@ -1,14 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainLayoutComponent {
-
-}
+export class MainLayoutComponent {}
