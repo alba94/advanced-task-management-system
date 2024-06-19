80f68b137665b8f2597041d3925027ad416005d5
alba <alballubani19@gmail.com>
2024-06-19 23:20:27 +0200

    added delete task dialog

--- src/app/routes/task-manager/components/delete-task-dialog/delete-task-dialog.component.ts
+++ src/app/routes/task-manager/components/delete-task-dialog/delete-task-dialog.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { TaskFormComponent } from '../task-form/task-form.component';
 
 @Component({
   selector: 'app-delete-task-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+    TaskFormComponent,
+  ],
   templateUrl: './delete-task-dialog.component.html',
-  styleUrl: './delete-task-dialog.component.css'
+  styleUrl: './delete-task-dialog.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeleteTaskDialogComponent {
+  readonly dialogRef = inject(MatDialogRef<DeleteTaskDialogComponent>);
 
+  confirm(closed: boolean): void {
+    this.dialogRef.close(closed);
+  }
 }
